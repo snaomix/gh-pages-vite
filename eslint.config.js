8164b9b2c8a09d8995a47b69f51e184dc402f455
Naomi Shigemori <mnaomix@gmail.com>
2025-08-26 13:59:43 +0900

    Update ESLint configuration and bump package versions

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
 import html from "@html-eslint/eslint-plugin";
-import htmlParser from "@html-eslint/parser";
 import js from "@eslint/js";
 import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
+    ...js.configs.recommended,
     files: ['**/*.js'],
-    rules: {
-      ...js.configs.recommended.rules,
-    },
     languageOptions: {
       sourceType: "module",
       globals: {
@@ -19,20 +16,18 @@ export default [
     },
   },
   {
-    ...html.configs["flat/recommended"],
+    ...html.configs.recommended,
     files: ["**/*.html"],
     plugins: {
-      "@html-eslint": html,
+      html,
     },
+    language: "html/html",
     rules: {
-      ...html.configs["flat/recommended"].rules,
-      "@html-eslint/attrs-newline": "off",
-      "@html-eslint/indent": ["error", 2],
-      "@html-eslint/require-closing-tags": ["error"],
-      "@html-eslint/no-extra-spacing-attrs": ["error", { "enforceBeforeSelfClose": true }],
-    },
-    languageOptions: {
-      parser: htmlParser,
+      ...html.configs.recommended.rules,
+      "html/attrs-newline": "off",
+      "html/indent": ["error", 2],
+      "html/require-closing-tags": ["error"],
+      "html/no-extra-spacing-attrs": ["error", { "enforceBeforeSelfClose": true }],
     },
   },
   eslintConfigPrettier,
