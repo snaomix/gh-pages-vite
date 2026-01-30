8c4b7a7e5fd243b9406bd363a2bf477562a6e5d7
Naomi Shigemori <mnaomix@gmail.com>
2026-01-30 13:37:33 +0900

    Build: update Sass and suppress @import deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
+import { defineConfig } from "vite";
+
 /** @type {import('vite').UserConfig} */
-export default {
+export default defineConfig({
   root: "src",
   base: "/gh-pages-vite/",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        quietDeps: true,
+        silenceDeprecations: ["import"],
+      },
+    },
+  },
   server: {
     port: 8080,
   },
-};
+});
